25917060a4fdd31b0b2d78e2635de429d1abd363
ibill-mlearner <ibill792@uwsp.edu>
2025-07-18 20:16:53 -0500

    Update runner.py
    Add exit path via keyboard

--- runner.py
+++ runner.py
@@ -5,4 +5,7 @@ def main():
     app.run(debug=True, host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n[!] Server stopped.")
